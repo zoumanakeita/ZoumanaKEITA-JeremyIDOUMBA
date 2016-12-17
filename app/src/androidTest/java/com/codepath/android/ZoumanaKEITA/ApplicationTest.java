73c399c1f12f5936a8448357edd7895aaf60369c
zoumana <zoumana930@hotmail.fr>
2016-12-17 17:28:10 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/codepath/android/ZoumanaKEITA/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.codepath.android.ZoumanaKEITA;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+/**
+ * @author: Zoumana KEITA
+ * */
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
